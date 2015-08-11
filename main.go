3353092349656f58adb05ec5f1c84d88124b29f7
Jesse Lucas <jesse@forestgiant.com>
2015-08-11 19:13:21 -0400

    Checking to see if the weekday directory exist, if it doesn't create directory with 0600 permissions.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	// "os"
+	"os"
 	// "os/exec"
 	"time"
 )
@@ -19,9 +19,16 @@ func main() {
 	//Get the name of the directory where we want to save this backup
 	weekdays := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 	directory := weekdays[time.Now().Weekday()]
-	fmt.Println("Creating directory named", directory)
 
 	//Create the backup directory if it does not exist
+	fi, err := os.Stat(directory)
+	if err != nil {
+		fmt.Println("Creating directory named", directory)
+		// create folder
+		os.Mkdir(directory, 0600)
+	} else {
+		fmt.Println("Folder exists!: ", fi.Name())
+	}
 
 	//Define our openshift command
 
