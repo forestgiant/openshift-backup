104d4b4885bf241dddd5dedaafb063c02ed6043e
Jesse Lucas <jesse@forestgiant.com>
2015-08-11 20:52:32 -0400

    Setting up user's home directory path. Creating a function to create directories. Creating folder to store all back ups in.

--- main.go
+++ main.go
@@ -4,32 +4,63 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/user"
 	// "os/exec"
+	"log"
 	"time"
 )
 
 func main() {
+	// Get Users home directory
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(user.HomeDir)
+
 	//Check for our command line configuration flags
 	var (
-		backupPathPtr = flag.String("backupPath", "~/", "The base directory where the openshift backups will be stored.")
+		backupPathPtr = flag.String("backupPath", user.HomeDir, "The base directory where the openshift backups will be stored.")
 	)
 	flag.Parse()
 	fmt.Println("Running openshift-backup with backup path set to ", *backupPathPtr)
+
+	// Set Path
+	path := *backupPathPtr + "/" + "OpenShiftBackUps"
+
+	// Create OpenShiftBackUps directory
+	createDir(path, 0700)
 
 	//Get the name of the directory where we want to save this backup
 	weekdays := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 	directory := weekdays[time.Now().Weekday()]
 
+	dirPath := path + "/" + directory
+
 	//Create the backup directory if it does not exist
-	fi, err := os.Stat(directory)
-	if err != nil {
-		fmt.Println("Creating directory named", directory)
-		// create folder
-		os.Mkdir(directory, 0600)
-	} else {
-		fmt.Println("Folder exists!: ", fi.Name())
-	}
+	createDir(dirPath, 0600)
 
 	//Define our openshift command
 
+}
+
+func createDir(name string, perm os.FileMode) error {
+	fi, err := os.Stat(name)
+	if err != nil {
+		fmt.Println("Creating directory named", name)
+
+		// create folder
+		err = os.Mkdir(name, perm)
+		if err != nil {
+			fmt.Println("Couldn't create directory: ", err)
+
+			return err
+		}
+
+	} else {
+		fmt.Println("Folder exists!: ", fi.Name())
+
+	}
+
+	return nil
 }
